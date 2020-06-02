f6e5aaf0463bdbb1558218c46a5e40534f5d6bfc
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-06-02 09:59:30 +0100

    added bomb and flag emojis

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Cell, CellState, CellValue } from '../types'
+import { CellState, CellValue } from '../types'
 
 interface ButtonProps {
-  row: number, 
+  row: number,
   col: number,
   state: CellState,
   value: CellValue
 }
 
-export const Button: React.FC <ButtonProps> = ({ row, col, state, value }) => (
-  <div className="Button"></div>
-)
+export const Button: React.FC<ButtonProps> = ({ row, col, state, value }) => {
+  const renderContent = (): React.ReactNode => {
+    if (state === CellState.visible) {
+      if (value === CellValue.bomb) {
+        return <span role="img" aria-label="Bomb">💣</span>
+      } else {
+        return value
+      }
+    } else if (state === CellState.flagged) {
+      return <span role="img" aria-label="Flag">🚩</span>
+    } else {
+      return null
+    }
+  }
+  return (
+    <div className={`Button ${state === CellState.visible ? 'visible' : ''}`}>
+      {renderContent()}
+    </div>
+  )
+}
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,33 +1,34 @@
-import { CONSTANTS } from '../constants'
-import { CellValue, CellState, Cell } from '../types'
+import { CONSTANTS } from "../constants";
+import { CellValue, CellState, Cell } from "../types";
 
 export const generateCells = (): Cell[][] => {
   //an array of different Cells, wrapped in an array
-  let cells: Cell[][] = []
+  let cells: Cell[][] = [];
 
   //generate all cells
   for (let row = 0; row < CONSTANTS.MAX_ROWS; row++) {
-    cells.push([])
+    cells.push([]);
     for (let col = 0; col < CONSTANTS.MAX_COLS; col++) {
       cells[row].push({
         value: CellValue.none,
-        state: CellState.initial
-      })
+        state: CellState.visible //TODO: switch back to Cellstate.initial
+      });
     }
   }
 
   //randomly add bombs
-  let bombsPlaced = 0 
-  while (bombsPlaced < CONSTANTS.NO_OF_BOMBS) {
-    const randomRow = Math.floor(Math.random() * CONSTANTS.MAX_ROWS)
-    const randomCol = Math.floor(Math.random() * CONSTANTS.MAX_COLS)
-    const currentCell = cells[randomRow][randomCol]
+  let bombsPlaced = 0;
+  while (bombsPlaced <= CONSTANTS.NO_OF_BOMBS) {
+    const randomRow = Math.floor(Math.random() * CONSTANTS.MAX_ROWS);
+    const randomCol = Math.floor(Math.random() * CONSTANTS.MAX_COLS);
+    const currentCell = cells[randomRow][randomCol];
     if (currentCell.value !== CellValue.bomb) {
       cells[randomRow][randomCol] = {
-        ...cells[randomRow][randomCol], value: CellValue.bomb
-      }
+        ...cells[randomRow][randomCol],
+        value: CellValue.bomb
+      };
     }
-    bombsPlaced++
+    bombsPlaced++;
   }
-  return cells
-}
+  return cells;
+};
