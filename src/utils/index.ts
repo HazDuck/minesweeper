8aaa6c6f8d5b0de1a181fbc313857613fda604cc
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-06-03 11:44:39 +0100

    number of bombs logic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,7 +18,7 @@ export const generateCells = (): Cell[][] => {
 
   //randomly add bombs
   let bombsPlaced = 0;
-  while (bombsPlaced <= CONSTANTS.NO_OF_BOMBS) {
+  while (bombsPlaced < CONSTANTS.NO_OF_BOMBS) {
     const randomRow = Math.floor(Math.random() * CONSTANTS.MAX_ROWS);
     const randomCol = Math.floor(Math.random() * CONSTANTS.MAX_COLS);
     const currentCell = cells[randomRow][randomCol];
@@ -27,17 +27,43 @@ export const generateCells = (): Cell[][] => {
         ...cells[randomRow][randomCol],
         value: CellValue.bomb
       };
+      bombsPlaced++;
     }
-    bombsPlaced++;
   }
   //calculate the numbers for each cell
-  
-  for (let rowIndex = 0; rowIndex < CONSTANTS.MAX_ROWS; rowIndex++ ) {
+
+  for (let rowIndex = 0; rowIndex < CONSTANTS.MAX_ROWS; rowIndex++) {
     for (let colIndex = 0; colIndex < CONSTANTS.MAX_COLS; colIndex++) {
-      
+      const currentCell = cells[rowIndex][colIndex];
+      if (currentCell.value === CellValue.bomb) {
+        continue;
+      }
+      let numberOfBombs = 0;
+      const topLeftBomb =
+        rowIndex > 0 && colIndex > 0 ? cells[rowIndex - 1][colIndex - 1] : null;
+      const topBomb = rowIndex > 0 ? cells[rowIndex - 1][colIndex] : null;
+      const topRightBomb =
+        rowIndex > 0 && colIndex < CONSTANTS.MAX_COLS - 1
+          ? cells[rowIndex - 1][colIndex + 1]
+          : null;
+      const leftBomb = colIndex > 0 ? cells[rowIndex][colIndex - 1] : null;
+      const rightBomb =
+        colIndex < CONSTANTS.MAX_COLS - 1
+          ? cells[rowIndex][colIndex + 1]
+          : null;
+      const bottomLeftBomb =
+        rowIndex < CONSTANTS.MAX_ROWS - 1 && colIndex > 0
+          ? cells[rowIndex + 1][colIndex - 1]
+          : null;
+      const bottomBomb =
+        rowIndex < CONSTANTS.MAX_ROWS - 1
+          ? cells[rowIndex + 1][colIndex]
+          : null;
+      const bottomRightBomb =
+        rowIndex < CONSTANTS.MAX_ROWS - 1 && colIndex < CONSTANTS.MAX_COLS - 1
+          ? cells[rowIndex + 1][colIndex + 1]
+          : null;
     }
   }
-
   return cells;
 };
-
