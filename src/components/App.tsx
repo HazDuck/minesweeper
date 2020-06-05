d6d4c9746166f84a5d35fb273f43cd31cd519df5
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-06-05 12:36:00 +0100

    face change on mousedown/up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NumberDisplay } from './NumberDisplay'
 import { generateCells } from '../utils'
 import { Button } from './Button'
+import { Face } from '../types'
 
 export const App: React.FC = () => {
   //dont forget you can initialize useState with a called funciton 😁
-  const [cells, setCells ] = useState(generateCells())
+  const [cells, setCells] = useState(generateCells())
+  const [face, setFace] = useState(Face.smile)
+
+  useEffect(() => {
+    window.addEventListener('mousedown', () => {
+      setFace(Face.oh)
+    })
+    window.addEventListener('mouseup', () => {
+      setFace(Face.smile)
+    })
+  }, [])
 
   return (
     <div className="App">
@@ -15,7 +26,7 @@ export const App: React.FC = () => {
         <span
           role="img"
           aria-label="Smiley emoji"
-        >😁</span>
+        >{face}</span>
       </div>
       <NumberDisplay value={23} />
       </div>
