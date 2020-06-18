f96981fd566b253885075645ceb73a55dc95bed4
Pete Heyes <peteheyes@Leighs-MBP-4.lan>
2020-06-18 18:18:15 +0100

    minus formatting

--- src/components/NumberDisplay.tsx
+++ src/components/NumberDisplay.tsx
@@ -7,7 +7,7 @@ interface NumberDisplayProps {
 export const NumberDisplay: React.FC <NumberDisplayProps> = ({ value }) => {
   return (
     <div className="NumberDisplay">
-      { value < 0 ? value : value.toString().padStart(3, '0')}
+      { value < 0 ? `-${Math.abs(value).toString().padStart(2 ,'0')}` : value.toString().padStart(3, '0')}
     </div>
   )
 }
